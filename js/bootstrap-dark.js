f0c2350bc41674db13797faabc7ef43bc6ff100f
ravi <ravilamkoti@gmail.com>
2020-02-20 19:51:32 +0530

    fixed: js

--- js/bootstrap-dark.js
+++ js/bootstrap-dark.js
@@ -1,9 +1,14 @@
 const btn = document.getElementById('switch-theme');
+localStorage.setItem('theme','light');
 btn.addEventListener('click',(e)=>{
-     if(btn.checked){
+     let theme = localStorage.getItem('theme');
+     console.log(theme);
+     if(theme == 'light' || theme ==''){
           document.documentElement.setAttribute('data-theme','dark');
+          localStorage.setItem('theme','dark');
      }
      else {
           document.documentElement.removeAttribute('data-theme');
+          localStorage.setItem('theme','light');
      }
-});
+})
